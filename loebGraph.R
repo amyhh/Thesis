b57d9dc0d69cabe2ad8daedade1e5736feb4313b
Amy <97184132+amyhh@users.noreply.github.com>
2024-10-02 19:25:50 -0400

    Update loebGraph.R
    - cleaned up plot code, commented out library and installs
    - added log scale plot

--- loebGraph.R
+++ loebGraph.R
@@ -3,8 +3,10 @@
 
 #with forestplot package - have questions about why the black bars are so big
 ## install.packages("forestplot")
-library("forestplot")
-library(dplyr)
+#library('dplyr')
+#library(tidyverse)
+#library(gt)
+#library(ggplot2)
 
 #Hazard ratios 
 
@@ -15,39 +17,35 @@ upper.CI <- c(10.72, 5.49, 8.98, 1.50, 1.69)
 data <- data.frame(country.name, HR, lower.CI, upper.CI)
 print(data)
 
-forestplot(labeltext = "Canada", mean = 2.83, lower = 0.75, upper = 10.72)
-
-forestplot(labeltext = data$country.name, mean = c(1,1,1,1,1), lower = data$lower.CI, upper = data$upper.CI)
-data$country.name <- as.factor(data$country.name)
-
-
 ######################################################################
 
 ## Think about what you would need to pass to make this a separate script
 
 #using ggplot2
-library(tidyverse)
-library(gt)
-library(ggplot2)
-## help(fct_rev)
 p <- 
   data |>
   ggplot(aes(y = fct_rev(country.name))) + 
-  theme_classic()
-print(p)
-
-
-p <- p + geom_point(aes(x=data$HR), shape=15, size=3) +
-  geom_linerange(aes(xmin=data$lower.CI, xmax=data$upper.CI)) 
-p
-
-p <- p +
+  theme_classic() +
+  geom_point(aes(x=data$HR), shape=15, size=3) +
+  geom_linerange(aes(xmin=data$lower.CI, xmax=data$upper.CI)) +
+  scale_x_continuous(breaks=seq(0,11,by=2)) +
   geom_vline(xintercept = 2, linetype="dashed") +
-  labs(x="Hazard Ratio", y="Country")
+  labs(title = "Hazard Ratio of Covid-19 Infection for Surgical Masks vs.Respirator", x="Hazard Ratio (95% CI)", y="Country") +
+  coord_cartesian(ylim=c(1,5), xlim=c(0, 11)) +
+  annotate("text", x = 1.80, y = 0.5, label = "HR = 2") 
 p
 
-p <- p +
-  coord_cartesian(ylim=c(1,5), xlim=c(-1, 11)) + 
-  annotate("text", x = 2, y = 0.5, label = "HR = 2")
-p
+##log scale
 
+plog <- 
+  data |>
+  ggplot(aes(y = fct_rev(country.name))) + 
+  theme_classic() +
+  geom_point(aes(x=data$HR), shape=15, size=3) +
+  geom_linerange(aes(xmin=data$lower.CI, xmax=data$upper.CI)) +
+  scale_x_log10(breaks=seq(1,11,by=2)) +
+  geom_vline(xintercept = 2, linetype="dashed") +
+  labs(title = "Hazard Ratio of Covid-19 Infection for Surgical Masks vs.Respirator", x="Hazard Ratio (95% CI)", y="Country") +
+  #coord_cartesian(ylim=c(1,5), xlim=c(0, 11)) +
+  annotate("text", x = 1.80, y = 0.5, label = "HR = 2") 
+plog
