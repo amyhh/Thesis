8435efc6e1fda14227bf22bf3c187df984bb1b49
Amy <97184132+amyhh@users.noreply.github.com>
2024-12-18 23:21:38 -0500

    Update pubmed.R
    added better code to format the txt files

--- pubmed.R
+++ pubmed.R
@@ -4,11 +4,13 @@
   #note that earlier versions of easyPubMed install when I've used the general install("easyPubMed)
   #the following code will only run on the later (>3.0) versions
 library(easyPubMed)
+library(tidyverse)
 
 my_query <- readLines("bioTerms.txt")
-my_query <- paste(my_query, collapse = "\t") 
+my_query <- str_flatten(query)
 my_query <- stringr::str_squish(my_query) #these two lines turn human-readable txt format into something to input into easyPubMed
 my_query
+
 epm <- epm_query(my_query)
 epm <- epm_fetch(epm, format = 'medline', write_to_file = TRUE, outfile_prefix = "testPubMedDec11", store_contents = TRUE)
   #this is now a nice medline file that is human readable 
