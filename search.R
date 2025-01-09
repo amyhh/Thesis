a7d35fd1fe494b74b304e184138574c8db472d4a
Jonathan Dushoff <dushoff@mcmaster.ca>
2025-01-09 16:48:21 -0500

    rpcall

--- search.R
+++ search.R
@@ -2,7 +2,7 @@
 library(shellpipes)
 ## If you change the name of the text file, this should a) just work;
 ## b) write the output to a corresponding output file, making it easier to keep track of what goes with what
-rpcall("simple.search.Rout simple.pipestar search.R simple.txt")
+rpcall("title.search.Rout title.pipestar search.R title.search")
 
 library(easyPubMed)
 library(readr)
